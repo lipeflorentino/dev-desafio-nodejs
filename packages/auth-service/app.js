139154b93512d3ba8bd9fc338a6bab84df47494e
lipeflorentino2 <lipeflorentino2@gmail.com>
2024-06-16 23:59:18 -0300

    feat: add openAPI documentation and swagger-ui

--- packages/auth-service/app.js
+++ packages/auth-service/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRoutes = require('./src/routes/authRoutes');
+const swaggerSetup = require('./swagger');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use('/auth', authRoutes);
+swaggerSetup(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
