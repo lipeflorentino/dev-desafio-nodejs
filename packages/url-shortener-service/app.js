9459bf9b925e6839b861938e13c5c3893b6d9adf
lipeflorentino2 <lipeflorentino2@gmail.com>
2024-06-17 00:00:02 -0300

    feat: add openAPI documentation and swagger-ui to url service

--- packages/url-shortener-service/app.js
+++ packages/url-shortener-service/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const urlRoutes = require('./src/routes/urlRoutes');
+const swaggerSetup = require('./swagger');
 
 const app = express();
 const port = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use('/url', urlRoutes);
+swaggerSetup(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
